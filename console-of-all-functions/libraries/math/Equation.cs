5849d163d2da900a563d8ee73569aa79fec0982a
André Soares <andre.franca@tce.ro.gov.br>
2019-05-06 12:26:58 -0400

    Some removals to make the exe lighter

---
+++ console-of-all-functions/libraries/math/Equation.cs
@@ -0,0 +1,10 @@
+﻿namespace console_of_all_functions.libraries.math
+{
+    class Equation
+    {
+        internal static void Calculate(string expression)
+        {
+            
+        }
+    }
+}
